ed4bd69097b9890669d194f11af8ed4e967ebc82
Ayushrai83 <gurukul4ayush@gmail.com>
2024-07-19 14:29:03 +0530

    Frontend updates in components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,40 +2,6 @@ import Notes from "./Notes";
 export const Home = () => {
   return (
     <div className="container">
-      <div className="container my-3">
-        <h2>Add a Note</h2>
-        <form className="my-3">
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-
       <Notes />
     </div>
   );
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -22,9 +22,32 @@ const NoteState = (props)=>{
         }
       ]
       const [notes, setNotes] = useState(notesInitial)
-    
+
+      //Add a note
+      const addNOte = (title,description,tag) => {
+        note  = {
+          "_id": "66954b319597cf193a735ccc",
+          "user": "6693f1c793be8dca57ef4222",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2024-07-15T16:15:45.681Z",
+          "__v": 0
+        }
+      
+        setNotes(notes.concat(note))
+      }
+      //Delete a note
+      const deleteNote = () => {
+
+      }
+
+      //Edit a note
+      const editNote = () => {
+        
+      }
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNOte, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
